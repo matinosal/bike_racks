8c83e286dc3fed55b10bbcf2d1a737a6813da7e9
Mateusz Nosal <mateusz.nosal@vp.pl>
2022-12-04 00:14:01 +0100

    fix: User initial location is shown properly

--- src/views/MapScreen.tsx
+++ src/views/MapScreen.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import MapView, { LatLng, Marker, Region } from "react-native-maps";
+import MapView, {
+  EventUserLocation,
+  LatLng,
+  Marker,
+  Region,
+} from "react-native-maps";
 import * as Location from "expo-location";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   MapCoordinates,
   MapDelta,
@@ -19,11 +24,12 @@ const initialData: MapLocation = {
   longitudeDelta: 0.002297312021255493,
 };
 
+let blockRegionChange = false;
 const markerProvider = new MarkerProvider();
 
 const MapScreen: React.FC = () => {
-  const [location, setLocation] = useState<MapCoordinates>(initialData);
-  const [locationDelta, setLocationDelta] = useState<MapDelta>(initialData);
+  const mapRef = useRef<MapView>(null);
+
   const [errorMessage, setErrorMessage] = useState<String>();
   const [markers, setMarkers] = useState<MarkerType[]>([]);
 
@@ -32,13 +38,9 @@ const MapScreen: React.FC = () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMessage("Permission to access location was denied");
-        setLocation(initialData);
+        blockRegionChange = true;
         return;
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location.coords);
-      setLocationDelta(initialData);
     })();
   }, []);
 
@@ -55,13 +57,32 @@ const MapScreen: React.FC = () => {
       setMarkers(providedMarkers);
     }
   };
-
+  const userLocationChangeHandler = (location: EventUserLocation) => {
+    if (blockRegionChange == false) {
+      mapRef.current?.animateToRegion(
+        {
+          latitude: location.nativeEvent.coordinate.latitude,
+          longitude: location.nativeEvent.coordinate.longitude,
+          latitudeDelta: initialData.latitudeDelta,
+          longitudeDelta: initialData.longitudeDelta,
+        },
+        1
+      );
+      blockRegionChange = true;
+    }
+  };
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         provider="google"
+        showsUserLocation={true}
+        pitchEnabled={false}
         initialRegion={initialData as Region}
+        onUserLocationChange={(event) => {
+          userLocationChangeHandler(event);
+        }}
         onRegionChangeComplete={(region, isGesture) => {
           onRegionChangeCompleteHandler(region, isGesture);
         }}
